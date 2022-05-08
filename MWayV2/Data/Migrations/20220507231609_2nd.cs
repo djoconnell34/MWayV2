f5d6b5ddd88661154b7bb9d227a039aa8b4f9aca
Daniel O'Connell <djoconnell34@gmail.com>
2022-05-08 08:44:07 -0400

    Add project files.

---
+++ MWayV2/Data/Migrations/20220507231609_2nd.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MWayV2.Data.Migrations
+{
+    public partial class _2nd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AddressCity",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressStreet",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressZip",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "ProfilePicture",
+                schema: "Identity",
+                table: "User",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkCity",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkStreet",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WorkZip",
+                schema: "Identity",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddressCity",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "AddressStreet",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "AddressZip",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "WorkCity",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "WorkStreet",
+                schema: "Identity",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "WorkZip",
+                schema: "Identity",
+                table: "User");
+        }
+    }
+}
---
+++ MWayV2/Models/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MWayV2.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = "";
+
+        public string LastName { get; set; } = "";
+
+        public string? AddressStreet { get; set; }
+
+        public string? AddressCity { get; set; }
+
+        public string? AddressZip { get; set; }
+
+        public string? WorkStreet { get; set; }
+
+        public string? WorkCity { get; set; }
+
+        public string? WorkZip { get; set; }
+
+        public byte[]? ProfilePicture { get; set; }
+    }
+}

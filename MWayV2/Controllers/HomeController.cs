2933dc939e52f44c3f5aeb4cdbbc4aeb129bff29
Daniel O'Connell <djoconnell34@gmail.com>
2022-05-10 10:27:51 -0400

    Budget Chart

--- MWayV2/Controllers/HomeController.cs
+++ MWayV2/Controllers/HomeController.cs
@@ -1,32 +1,83 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MWayV2.Data;
 using MWayV2.Models;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace MWayV2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+
+        public async Task<ActionResult> Index()
         {
-            return View();
+            ClaimsPrincipal currentUser = this.User;
+            var currentUserID = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            var budGroupCar = "Car";
+            var dataCar = _context.budgets.Where(x => x.IdHolder.Contains(currentUserID) && x.BudgetGroup == budGroupCar).Sum(x => x.BudgetItemCost);
+            
+
+            var budGroupHome = "Home";
+            var dataHome = _context.budgets.Where(x => x.IdHolder.Contains(currentUserID) && x.BudgetGroup == budGroupHome).Sum(x => x.BudgetItemCost);
+            
+
+            var budGroupElectronics = "Electronics";
+            var dataElect = _context.budgets.Where(x => x.IdHolder.Contains(currentUserID) && x.BudgetGroup == budGroupElectronics).Sum(x => x.BudgetItemCost);
+            
+
+            var budGroupOther = "Other";
+            var dataOther = _context.budgets.Where(x => x.IdHolder.Contains(currentUserID) && x.BudgetGroup == budGroupOther).Sum(x => x.BudgetItemCost);
+
+            return View(dataCar);
         }
 
-        public IActionResult Privacy()
+        public ActionResult GetData()
         {
-            return View();
+            ClaimsPrincipal currentUser = this.User;
+            var currentUserID = currentUser.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            var budGroupCar = "Car";
+            var dataCar = _context.budgets.Where(x => x.IdHolder.Contains(currentUserID) && x.BudgetGroup == budGroupCar).Sum(x => x.BudgetItemCost);
+
+
+            var budGroupHome = "Home";
+            var dataHome = _context.budgets.Where(x => x.IdHolder.Contains(currentUserID) && x.BudgetGroup == budGroupHome).Sum(x => x.BudgetItemCost);
+
+
+            var budGroupElectronics = "Electronics";
+            var dataElectron = _context.budgets.Where(x => x.IdHolder.Contains(currentUserID) && x.BudgetGroup == budGroupElectronics).Sum(x => x.BudgetItemCost);
+
+
+            var budGroupOther = "Other";
+            var dataOther = _context.budgets.Where(x => x.IdHolder.Contains(currentUserID) && x.BudgetGroup == budGroupOther).Sum(x => x.BudgetItemCost);
+
+            percent obj = new percent();
+            obj.car = (double)dataCar;
+            obj.home = (double)dataHome;
+            obj.electron = (double)dataElectron;
+            obj.other = (double)dataOther;
+
+
+
+            return Json(obj);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public class percent
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            public double car { get; set; }
+            public double home { get; set; }
+            public double electron { get; set; }
+            public double other { get; set; }
         }
+
     }
 }
